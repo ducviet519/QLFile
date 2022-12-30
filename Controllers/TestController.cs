e25978165671c004f7997b364cbae39f80903cc8
DucViet519 <ducviet519@gmail.com>
2022-12-30 13:19:56 +0700

    ok

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebTools.Services;
 
 namespace WebTools.Controllers
 {
     public class TestController : Controller
     {
+        private readonly IUnitOfWork _services;
+        public TestController(IUnitOfWork services) 
+        {
+            _services = services;
+        }
         public IActionResult Index()
         {
 
             return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetData()
+        {
+            var data = await _services.Report_List.GetReportListAsync();
+            return Json( new { data = data });
         }
     }
 }
