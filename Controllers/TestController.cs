c1d29320edc40ff3a22bfca859abe66c5c960397
DucViet519 <ducviet519@gmail.com>
2022-12-31 11:23:21 +0700

    xong test easyUI

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebTools.Models;
 using WebTools.Services;
 
 namespace WebTools.Controllers
@@ -18,25 +21,7 @@ namespace WebTools.Controllers
         }
         public IActionResult Index()
         {
-
             return View();
-        }
-
-        [HttpGet]
-        public async Task<JsonResult> GetData2(int page = 1, int rows = 10)
-        {
-            var data = await _services.Report_List.GetReportListAsync();
-            var pageIndex = Convert.ToInt32(page) - 1;
-            var pageSize = rows;
-            var totalRecords = data.Count();
-            var totalPages = (int)Math.Ceiling((float)totalRecords / (float)pageSize);
-            data = data.Skip(pageIndex * pageSize).Take(pageSize).ToList();
-            var jsonData = new
-            {
-                total = totalPages,
-                rows = data
-            };
-            return Json(new { data = jsonData });
         }
 
         [HttpPost]
@@ -50,6 +35,14 @@ namespace WebTools.Controllers
             data = data.Skip(pageIndex * pageSize).Take(pageSize).ToList();
 
             return Json( new { rows = data, total = totalPages });
+        }
+
+        [HttpPost]
+        public JsonResult GetTreeData(int page = 1, int rows = 10)
+        {
+            string json = System.IO.File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Data\\treegrid2_data.json"));
+            Root data = JsonConvert.DeserializeObject<Root>(json);
+            return Json( new { rows = data.rows, total = data.total });
         }
     }
 }
---
+++ Models/Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTools.Models
+{
+    public class Test
+    {
+        
+    }
+    public class Footer
+    {
+        public string region { get; set; }
+        public int f1 { get; set; }
+        public int f2 { get; set; }
+        public int f3 { get; set; }
+        public int f4 { get; set; }
+        public int f5 { get; set; }
+        public int f6 { get; set; }
+        public int f7 { get; set; }
+        public int f8 { get; set; }
+    }
+
+    public class Root
+    {
+        public int total { get; set; }
+        public List<Row> rows { get; set; }
+        public List<Footer> footer { get; set; }
+    }
+
+    public class Row
+    {
+        public int id { get; set; }
+        public string region { get; set; }
+        public string state { get; set; }
+        public int? status { get; set; }
+        public int? action { get; set; }
+        public int? f1 { get; set; }
+        public int? f2 { get; set; }
+        public int? f3 { get; set; }
+        public int? f4 { get; set; }
+        public int? f5 { get; set; }
+        public int? f6 { get; set; }
+        public int? f7 { get; set; }
+        public int? f8 { get; set; }
+        public int? _parentId { get; set; }
+    }
+}
